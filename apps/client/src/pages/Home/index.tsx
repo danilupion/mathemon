231f3ff1fd5a48c4eeaa491dcd0a601935ccd96f
Dani Lupión <danilupion@gmail.com>
2022-12-28 23:14:09 +0100

    Fixed design regression from refactor

--- apps/client/src/pages/Home/index.tsx
+++ apps/client/src/pages/Home/index.tsx
@@ -3,8 +3,8 @@ import styles from './index.module.scss';
 const Home = () => {
   return (
     <div className={styles.container}>
-      <i className="nes-ash"></i>
-      <div className="nes-balloon from-left">
+      <i></i>
+      <div className="">
         ¡Aprendamos mates! Seleciona las operaciones que quieres practicar en el menú de la
         izquierda, en el menú de la derecha podrás seleccionar la dificultad.
       </div>
