2b7bef63abaadcc73229b60c013601099ad0cd06
Dani Lupión <danilupion@gmail.com>
2022-12-30 19:58:04 +0100

    Home page design improvements

--- apps/client/src/pages/Home/index.tsx
+++ apps/client/src/pages/Home/index.tsx
@@ -3,11 +3,11 @@ import styles from './index.module.scss';
 const Home = () => {
   return (
     <div className={styles.container}>
-      <i></i>
       <div className="">
         ¡Aprendamos mates! Seleciona las operaciones que quieres practicar en el menú de la
         izquierda, en el menú de la derecha podrás seleccionar la dificultad.
       </div>
+      <i></i>
     </div>
   );
 };
