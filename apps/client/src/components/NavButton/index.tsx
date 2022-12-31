4b2580fa48b275637993c37d4e0b92a2b3bafc01
Dani Lupión <danilupion@gmail.com>
2022-12-31 17:23:57 +0100

    Improved NavButton tu support regular anchors

--- apps/client/src/components/NavButton/index.tsx
+++ apps/client/src/components/NavButton/index.tsx
@@ -1,17 +1,33 @@
 import classNames from 'classnames';
-import { PropsWithChildren } from 'react';
+import { CSSProperties, PropsWithChildren } from 'react';
 import { NavLink, NavLinkProps } from 'react-router-dom';
 
 import styles from './index.module.scss';
 
-type NavButtonProps = Omit<NavLinkProps, 'children' | 'className'> &
-  PropsWithChildren<{ className?: string }>;
+type NavButtonNavLinkProps = Omit<NavLinkProps, 'children' | 'className' | 'style'> &
+  PropsWithChildren<{ native?: boolean; className?: string; style?: CSSProperties }>;
 
-const NavButton = ({ children, className, ...props }: NavButtonProps) => {
+type NavButtonAnchorProps = Omit<NavButtonNavLinkProps, 'to'> & { href: string };
+
+type NavButtonProps<Native extends boolean> = Native extends true
+  ? NavButtonAnchorProps
+  : NavButtonNavLinkProps;
+
+const NavButton = <Native extends boolean>({
+  children,
+  className,
+  native = false,
+  ...props
+}: NavButtonProps<Native>) => {
+  const Component = native ? 'a' : NavLink;
+
   return (
-    <NavLink className={classNames(styles['nav-button'], className)} {...props}>
+    <Component
+      className={classNames(styles['nav-button'], className)}
+      {...(props as unknown as NavButtonNavLinkProps)}
+    >
       {children}
-    </NavLink>
+    </Component>
   );
 };
 
