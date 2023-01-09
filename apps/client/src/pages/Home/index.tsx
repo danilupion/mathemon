ea726231b452bad0d5284d57154bc07748041c5c
Dani Lupión <danilupion@gmail.com>
2023-01-09 21:42:15 +0100

    Minor change in home page

--- apps/client/src/pages/Home/index.tsx
+++ apps/client/src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.module.scss';
 const Home = () => {
   return (
     <div className={styles.container}>
-      <div className="">
+      <div className={styles.speech}>
         ¡Aprendamos mates! Selecciona las operaciones que quieres practicar en el menú de la
         izquierda, en el menú de la derecha podrás seleccionar la dificultad.
       </div>
