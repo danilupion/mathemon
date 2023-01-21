1389e04c70f58e3180d98635b1d3aa08cfeccb7c
Dani Lupión <danilupion@gmail.com>
2023-01-21 19:16:39 +0100

    #12 Rework on quiz creation and approach change in divisions

--- apps/server/src/routes/api/quizzes/controllers.ts
+++ apps/server/src/routes/api/quizzes/controllers.ts
@@ -7,43 +7,145 @@ import controller, {
 import { StatusCode } from '@mathemon/turbo-server/http.js';
 import config from 'config';
 
-import { divisors, randomElement, randomInt } from '../../../utils/math.js';
+import { randomInt } from '../../../utils/math.js';
 
-interface CreateOperandParams {
+interface CreateAugendParams {
+  operator: Operator.addition;
   digits: number;
-  operator: Operator;
-  carrying?: boolean;
-  reference?: number;
+  carrying: boolean;
 }
+
+interface CreateAddendParams extends CreateAugendParams {
+  reference: number;
+}
+
+interface CreateMinuendParams {
+  operator: Operator.subtraction;
+  digits: number;
+  carrying: boolean;
+}
+
+interface CreateSubtrahendParams extends CreateMinuendParams {
+  reference: number;
+}
+
+interface CreateMultiplicandParams {
+  operator: Operator.multiplication;
+  digits: number;
+}
+
+interface CreateMultiplierParams extends CreateMultiplicandParams {
+  reference: number;
+}
+
+interface CreateDivisorParams {
+  operator: Operator.division;
+  digits: number;
+}
+
+interface CreateDividendParams extends CreateDivisorParams {
+  reference: number;
+}
+
+type CreateOperandParams =
+  | CreateAddendParams
+  | CreateAugendParams
+  | CreateMinuendParams
+  | CreateSubtrahendParams
+  | CreateMultiplicandParams
+  | CreateMultiplierParams
+  | CreateDivisorParams
+  | CreateDividendParams;
 
 const quizSize = config.get<number>('settings.quiz.size');
 
-const createOperand = ({ operator, digits, carrying, reference }: CreateOperandParams) => {
-  switch (operator) {
-    case Operator.addition:
-      if (reference !== undefined && carrying === false) {
-        const referenceDigits = reference.toString().split('');
+const createOperand = (params: CreateOperandParams) => {
+  switch (params.operator) {
+    case Operator.addition: {
+      const { digits, carrying } = params;
+      if ('reference' in params && !carrying) {
+        const referenceDigits = params.reference.toString().split('');
         const newDigits = referenceDigits.map((d) => randomInt(0, 9 - Number(d)).toString());
         return Number(newDigits.join(''));
       }
 
       return randomInt(0, 10 ** digits - 1);
-    case Operator.subtraction:
-      if (reference !== undefined && carrying === false) {
-        const referenceDigits = reference.toString().split('');
+    }
+    case Operator.subtraction: {
+      const { digits, carrying } = params;
+      if ('reference' in params && !carrying) {
+        const referenceDigits = params.reference.toString().split('');
         const newDigits = referenceDigits.map((d) => randomInt(0, Number(d)).toString());
         return Number(newDigits.join(''));
       }
 
-      return randomInt(0, reference === undefined ? 10 ** digits - 1 : reference);
-    case Operator.multiplication:
+      return randomInt(0, 'reference' in params ? params.reference : 10 ** digits - 1);
+    }
+    case Operator.multiplication: {
+      const { digits } = params;
       return randomInt(0, 10 ** digits - 1);
-    case Operator.division:
-      if (reference !== undefined) {
-        return randomElement(divisors(reference));
+    }
+    case Operator.division: {
+      const { digits } = params;
+      if ('reference' in params) {
+        return params.reference * randomInt(2, 10 ** digits - 1);
       }
 
-      return randomInt(1, 10 ** digits - 1);
+      return randomInt(2, 10 ** digits - 1);
+    }
+  }
+};
+
+interface CreateAdditionParams {
+  operator: Operator.addition;
+  digits: number;
+  carrying: boolean;
+}
+
+interface CreateSubtractionParams {
+  operator: Operator.subtraction;
+  digits: number;
+  carrying: boolean;
+}
+
+interface CreateMultiplicationParams {
+  operator: Operator.multiplication;
+  digits: number;
+}
+
+interface CreateDivisionParams {
+  operator: Operator.division;
+  digits: number;
+}
+
+type CreateOperationParams =
+  | CreateAdditionParams
+  | CreateSubtractionParams
+  | CreateMultiplicationParams
+  | CreateDivisionParams;
+
+const createOperation = (params: CreateOperationParams): Operation => {
+  switch (params.operator) {
+    case Operator.addition: {
+      const augend = createOperand({ ...params });
+      const addend = createOperand({ ...params, reference: augend });
+      return { operator: params.operator, operands: [augend, addend] };
+    }
+    case Operator.subtraction: {
+      const minuend = createOperand({ ...params });
+      const subthrahend = createOperand({ ...params, reference: minuend });
+      return { operator: params.operator, operands: [minuend, subthrahend] };
+    }
+    case Operator.multiplication: {
+      const multiplicand = createOperand({ ...params });
+      const multiplier = createOperand({ ...params });
+      return { operator: params.operator, operands: [multiplicand, multiplier] };
+    }
+    case Operator.division: {
+      const divisor = createOperand({ ...params });
+      const dividend = createOperand({ ...params, reference: divisor });
+      return { operator: params.operator, operands: [dividend, divisor] };
+    }
   }
 };
 
@@ -54,13 +156,8 @@ export const createQuiz = controller<
   const quizList: Operation[] = [];
 
   while (quizList.length < quizSize) {
-    const operand1 = createOperand({ ...req.body });
-    const operand2 = createOperand({
-      ...req.body,
-      reference: operand1,
-    });
+    const operation = createOperation(req.body as CreateOperationParams);
 
-    const operation: Operation = { operator: req.body.operator, operands: [operand1, operand2] };
     if (
       !quizList.some(
         (quiz) =>
